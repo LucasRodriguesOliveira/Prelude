432f3455ee48fc19c5fb8415c5cf4b3fef76b5de
NotDkwtc <ale-ringo@proton.me>
2026-07-10 22:24:04 +0200

    Feature: Added data types centralized header

---
+++ Core/data_types.hpp
@@ -0,0 +1,43 @@
+#ifndef DATA_TYPES_HPP
+#define DATA_TYPES_HPP
+
+#include <cstdint>  //std::int64_t, std::int32_t...
+#include <cstddef>  //std::size_t
+#include <memory>   //std::unique_ptr<>, std::shared_ptr<>, std::weak_ptr<>
+
+namespace Prelude{
+   namespace Core{
+      namespace Types{
+         using Int64   = std::int64_t;
+         using Int32   = std::int32_t;
+         using Int16   = std::int16_t;
+         using Int8    = std::int8_t;
+         using Int     = std::int_fast64_t;
+
+         using UInt64  = std::uint64_t;
+         using UInt32  = std::uint32_t;
+         using UInt16  = std::uint16_t;
+         using UInt8   = std::uint8_t;
+         using UInt    = std::uint_fast64_t;
+
+         using Float32 = float;
+         using Float64 = double;
+
+         using Char8   = char;
+         using Char16  = char16_t;
+         using Char32  = char32_t;
+
+         template <typename T>
+         using SmartPointer  = std::unique_ptr<T>;
+         template <typename T>
+         using SharedPointer = std::shared_ptr<T>;
+         template <typename T>
+         using WeakPointer   = std::weak_ptr<T>;
+
+         using Size   = std::size_t;
+      }
+   }
+}
+
+
+#endif  //!DATA_TYPES_HPP
